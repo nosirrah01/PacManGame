f8f493df57d410c03fc50b16083521a0b4de5658
Nosirrah01 <nosirrah01@gmail.com>
2023-04-04 15:48:26 -0400

    moved distance function from pellets.js to utilities.js

--- pellets.js
+++ pellets.js
@@ -80,13 +80,6 @@ function updateScaredTimer() {
     }
 }
 
-// Helper function to calculate the distance between two points
-function distance(x1, y1, x2, y2) {
-    const dx = x2 - x1;
-    const dy = y2 - y1;
-    return Math.sqrt(dx * dx + dy * dy);
-}
-
 function drawPellets() {
     ctx.fillStyle = 'white';
 
--- utilities.js
+++ utilities.js
@@ -7,3 +7,10 @@ function isWall(x, y) {
     // Check if it's a wall in the maze
     return maze[Math.floor(x / 32)][Math.floor(y / 32)];
 }
+
+// Helper function to calculate the distance between two points
+function distance(x1, y1, x2, y2) {
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    return Math.sqrt(dx * dx + dy * dy);
+}
