91e59c85603f829a39294e4d847bca9a5dc350a7
Nosirrah01 <nosirrah01@gmail.com>
2023-04-06 15:25:16 -0400

    left comment about what half image size is incase i wanna change it to that.

--- background.js
+++ background.js
@@ -2,5 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const body = document.body;
     const randomNumber = Math.floor(Math.random() * 10); // Generate a random number between 0 and 9.
     body.style.backgroundImage = `url('backgrounds/image${randomNumber}.jpeg')`;
+    // half image size is '364px 204px'
     body.style.backgroundSize = '728px 408px';
 });
